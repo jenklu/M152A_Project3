9f47206536c603c702f752e573d55a8726445449
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2017-05-03 17:54:05 -0700

    Add comments to explain clock logic

--- clk_div.v
+++ clk_div.v
@@ -18,6 +18,7 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+//TODO: Figure out how fast to make blink_clk blink
 module clk_div(
 //Input
 sys_clk, rst,
@@ -40,10 +41,11 @@ always@(posedge sys_clk) begin
     else begin
         fast_counter = counter + 1;
     end
-    //If fast_counter
+    //If fast_counter reaches 1 million, we reset it and set fast_clk high, so that it is high 100 times every second
     if(fast_counter == 19'd1000000) begin
         fast_clk = 1;
         fast_counter = 19'd0;
+        //we use count_fast_high so that we know when a full second has passed
         if(count_fast_high == 7'd101) begin
             count_fast_high = 0;
         end
@@ -54,14 +56,18 @@ always@(posedge sys_clk) begin
     else begin
         fast_clk = 0;
     end
+    //A full second has passed i.e. fast_clk has been high 100 times, so the 1Hz clk should go high and the 2Hz clk should go high
     if(count_fast_high == 7'd100) begin
         onehz_clk = 1;
         twohz_clk = 1;
     end
+    //The 2 Hz clk should go high 2x per second, so we have it go high ever 50 count_fast high (aka 50,000,000 clock ticks)
+    // i.e. at 50 count_fast_high AND 100 count_fast_high
     else if(count_fast_high == 7'd50) begin
         twohz_clk = 1;
         onehz_clk = 0;
     end
+    //Our clocks will only be high for one clock tick
     else
         twohz_clk = 0;
         onehz_clk = 0;
