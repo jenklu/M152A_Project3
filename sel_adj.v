1c8c195ae025a91b946fb436d08d97a4cb9bb08a
Jason Less <JasonLess@Jasons-MacBook-Pro.local>
2017-05-08 17:24:18 -0700

    fixed some syntax

--- sel_adj.v
+++ sel_adj.v
@@ -33,11 +33,12 @@ input twohz_clk;
 output reg [5:0] new_minutes;
 output reg [5:0] new_seconds;
 reg ignore;
-reg min_clk [5:0];
-reg sec_clk [5:0];
+reg min_clk;
+reg sec_clk;
 reg reach_60;
+
 counter min_counter(.clk(min_clk), .next(new_minutes), .reach60(ignore));
-counter sec_counter(.clk(sec_clk), .next(new_seconds), .reach60(reach_60);
+counter sec_counter(.clk(sec_clk), .next(new_seconds), .reach60(reach_60));
 	
 always @ (posedge clk) begin
     // If adj = 0, stopwatch behaves normally
