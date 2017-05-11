c81490e0e52179939bf1cc493ed4b6146b84e40e
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2017-05-10 17:12:58 -0700

    add blinking to display modules

--- display.v
+++ display.v
@@ -20,7 +20,7 @@ timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 module display(
 //Inputs
-min, sec, fast_clk,
+min, sec, fast_clk, blink_clk, sel, adj,
 //Outputs
 anode_vec, cathode_vec
     );
@@ -28,7 +28,7 @@ input [5:0] min;
 input [5:0] sec;
 input fast_clk;
 //anode_vec of form: [ AN3 AN2 AN1 AN0 ]
-output reg [3:0] anode_vec;
+output [3:0] anode_vec;
 //cathode_vec of form: [ CA CB CC CD CE CF CG ]
 output reg[6:0] cathode_vec;
 wire [3:0] digits [3:0];
@@ -40,6 +40,7 @@ assign digits[2] = min - (10*digits[3]);
 assign digits[0] = sec - (10*digits[1]);
 reg [1:0] switch_signal;
 reg[3:0] curr_dig;
+reg [3:0] anode_vec_temp;
 always @ (posedge fast_clk) begin
     if(switch_signal == 2'b11) begin
         switch_signal = 2'b00;
@@ -50,19 +51,19 @@ always @ (posedge fast_clk) begin
     case(switch_signal)
         0: begin
             curr_dig = digits[0];
-            anode_vec = 4'b0001;
+            anode_vec_temp = 4'b0001;
         end
         1: begin
             curr_dig = digits[1];
-            anode_vec = 4'b0010;
+            anode_vec_temp = 4'b0010;
         end
         2:  begin
             curr_dig = digits[2];
-            anode_vec = 4'b0100;
+            anode_vec_temp = 4'b0100;
         end
         3: begin
             curr_dig = digits[3];
-            anode_vec = 4'b1000;
+            anode_vec_temp = 4'b1000;
         end
     endcase
     case(curr_dig)
@@ -79,4 +80,21 @@ always @ (posedge fast_clk) begin
         default: cathode_vec = 7'b0110111
     endcase
 end
+reg [1:0] highlow;
+reg [3:0] mask;
+always @ posedge(blink_clk) begin
+    highlow = ~highlow;
+    //If adjusting seconds, make the seconds anodes blink
+    if(adj && sel) begin
+        mask = {1, 1, highlow};
+    end
+    //If adjusting minutes, make the minutes anodes blink
+    else if (adj && !sel) begin
+        mask = {highlow, 1, 1};
+    end
+    else begin
+        mask = 4'b1111;
+    end
+end
+assign anode_vec = anode_vec_temp & mask;
 endmodule
